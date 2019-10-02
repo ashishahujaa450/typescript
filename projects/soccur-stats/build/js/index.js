2a1042b46f8aaaef078b33fd2cb34b54266d0661
ashishahujaa450 <ahuja3594@gmail.com>
2019-10-02 13:27:55 +0530

    generate html file using another class

--- projects/soccur-stats/build/js/index.js
+++ projects/soccur-stats/build/js/index.js
@@ -2,11 +2,11 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var MatchReader_1 = require("./MatchReader");
 var CsvFileReader_1 = require("./CsvFileReader");
-var ConsoleReport_1 = require("./ReportTargets/ConsoleReport");
 var WinsAnalysis_1 = require("./Analyzers/WinsAnalysis");
 var Summary_1 = require("./Summary");
+var HtmlReports_1 = require("./ReportTargets/HtmlReports");
 var reader = new CsvFileReader_1.CsvFileReader('football.csv');
 var matchreader = new MatchReader_1.MatchReader(reader);
 matchreader.load();
-var summary = new Summary_1.Summary(new WinsAnalysis_1.WinAnalysis('Man United'), new ConsoleReport_1.ConsoleReport());
+var summary = new Summary_1.Summary(new WinsAnalysis_1.WinAnalysis('Man United'), new HtmlReports_1.HtmlReport());
 summary.buildAndPrintReport(matchreader.matches);
--- projects/soccur-stats/src/ts/index.ts
+++ projects/soccur-stats/src/ts/index.ts
@@ -4,11 +4,12 @@ import { CsvFileReader } from './CsvFileReader';
 import { ConsoleReport } from './ReportTargets/ConsoleReport';
 import { WinAnalysis } from './Analyzers/WinsAnalysis';
 import { Summary } from './Summary';
+import { HtmlReport } from './ReportTargets/HtmlReports';
 
 const reader = new CsvFileReader('football.csv');
 const matchreader = new MatchReader(reader);
 matchreader.load();
 
-const summary = new Summary(new WinAnalysis('Man United'), new ConsoleReport());
+const summary = new Summary(new WinAnalysis('Man United'), new HtmlReport());
 
 summary.buildAndPrintReport(matchreader.matches);
